302beff9b30b3560248d7ad5bf8939fbb9683935
Jonatan H Sundqvist <jonatanhsundqvist@gmail.com>
2014-12-21 16:27:30 +0100

    Added code in server.py and client.py

--- client.py
+++ client.py
@@ -14,4 +14,17 @@
 
 
 
-title
+import socket, sys               # Import socket module
+
+
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)		# Create a socket object
+host = socket.gethostname()	# Get local machine name
+port = 10000				# Reserve a port for your service.
+sock.bind((host, port))		# Bind to the port
+
+
+host = '192.168.1.88'
+print(host, port)
+sock.connect((host, port))
+sock.close
--- server.py
+++ server.py
@@ -14,4 +14,19 @@
 
 
 
-title
+import socket, sys               # Import socket module
+
+
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)		# Create a socket object
+host = socket.gethostname()	# Get local machine name
+port = 10000				# Reserve a port for your service.
+sock.bind(('192.168.1.88', port))		# Bind to the port
+
+sock.listen(5)                 # Now wait for client connection.
+
+while True:
+	connection, address = sock.accept()     # Establish connection with client.
+	print('Got connection from', address)
+	connection.send('Thank you for connecting')
+	connecting.close()
