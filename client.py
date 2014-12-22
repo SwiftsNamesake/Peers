4fc2b5f94d99223cc672c86d30bb4e8648522484
Jonatan H Sundqvist <jonatanhsundqvist@gmail.com>
2014-12-22 20:25:50 +0100

    1) Added threaded server, queue example 2) Deleted original sample code 3) Implemented simple illustration sharing

--- client.py
+++ client.py
@@ -6,7 +6,7 @@
 # December 21 2014
 #
 
-# TODO | -
+# TODO | - Promises (?)
 #        -
 #
 # SPEC | -
@@ -14,17 +14,164 @@
 
 
 
-import socket, sys               # Import socket module
+import socket, sys, time               # Import socket module
+import pickle
+import tkinter as tk
+import random
+import threading, queue
+
+from math import sin, cos, pi as π
+
+from PIL import Image
+from PIL import ImageTk
 
 
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)		# Create a socket object
-host = socket.gethostname()	# Get local machine name
-port = 10000				# Reserve a port for your service.
-sock.bind((host, port))		# Bind to the port
+def original():
+	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)		# Create a socket object
+	host = socket.gethostname()	# Get local machine name
+	port = 10000				# Reserve a port for your service.
+	sock.bind((host, port))		# Bind to the port
 
 
-host = '192.168.1.88'
-print(host, port)
-sock.connect((host, port))
-sock.close
+	host = '192.168.1.88'
+	print(host, port)
+	sock.connect((host, port))
+	sock.close
+
+
+
+def other():
+	# Sockets client script
+
+	world = []
+	x = 0
+
+	s = socket.socket()
+
+	print("Connecting to server...")
+
+	while True:
+		try:
+			s.connect(("localhost", 44000))
+			break
+		except:
+			print("Retrying connection to server...")
+			time.sleep(1)
+
+	print("Connection Matched!")
+
+	while True:
+		# keyboardInput = input("Enter the message: ")
+		# s.send(bytes(message, 'UTF-8'))
+		# message = keyboardInput.encode('utf-8')
+		# s.send(message)
+
+		update = []
+		for i in range(random.randint(1, 10)):
+			update.append((x, 40+20*sin(x*π/180.0)))
+			x += 5
+		world += update
+
+		# TODO: Protocol for sending smaller packets of pickled data in sequence
+		data = pickle.dumps(update)
+		s.send(bytes('{0:04d}'.format(len(data)), 'utf-8') + data) # Send data with padded length prefix
+
+		print("Message sent!")
+		# r = s.recv(512) #(128*10)
+		# a = r
+		# a = r.decode('utf-8')
+		# print("Confirmation received message: ", r)
+		# if keyboardInput == "quit":
+			# break
+		time.sleep(1)
+
+	print("Good bye")
+
+	s.close()
+
+	a = input("Enter key to End:")
+
+
+
+def connect(host, port):
+	
+	'''
+	Establish a connection with the server
+
+	'''
+
+	sock = socket.socket()
+	delay = 1.0
+
+	while True:
+		try:
+			print('Attempting to connect to {0:}'.format(host))
+			sock.connect((host, port))
+			print('Succeeded to connect.')
+			break
+		except:
+			print('Failed to connect to {0:}'.format(host))
+			print('Retrying after {0:} seconds'.format(delay))
+
+	return sock
+
+
+
+def main():
+	
+	'''
+	Application entry point
+
+	'''
+
+	SIZE = 720, 480
+	root = tk.Tk()
+	root.title('Client')
+	root.geometry('{0:}x{1:}'.format(*SIZE))
+	root.canvas = tk.Canvas(width=SIZE[0], height=SIZE[1])
+	root.canvas.pack()
+
+	updates = queue.Queue()
+
+	conn = connect('localhost', 44000)
+
+	def draw(event):
+		point = event.x, event.y
+		updates.put(point, False) # Do not block (?)
+
+		root.canvas.create_oval((event.x-10, event.y-10, event.x+10, event.y+10), fill='blue', width=0)
+
+	def cursor(event):
+		point = event.x, event.y
+		updates.put(('cur', point))
+
+	def upload():
+
+		'''
+		Uploads changes to the server
+
+		'''
+
+		points = []
+		while not updates.empty():
+			points.append(updates.get(False))
+		data = pickle.dumps(points)
+		conn.send(bytes('{0:04d}'.format(len(data)), 'utf-8') + data)
+
+		root.after(1000//30, upload) # TODO: Better model for pushing updates (?)
+
+	root.bind('<B1-Motion>', draw)
+	root.bind('<Motion>', cursor)
+	upload()
+
+
+	root.mainloop()
+
+
+
+
+
+if __name__ == '__main__':
+	main()
+	# other()
--- server.py
+++ server.py
@@ -15,18 +15,130 @@
 
 
 import socket, sys               # Import socket module
+import pickle
+
+import tkinter as tk
+from PIL import Image
+from PIL import ImageTk
+
+import random
+import threading, queue
 
 
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)		# Create a socket object
-host = socket.gethostname()	# Get local machine name
-port = 10000				# Reserve a port for your service.
-sock.bind(('192.168.1.88', port))		# Bind to the port
+SIZE = 720, 480
+root = tk.Tk()
+root.title('Server')
+root.geometry('{0:}x{1:}'.format(*SIZE))
+root.canvas = tk.Canvas(width=SIZE[0], height=SIZE[1])
+root.canvas.pack()
 
-sock.listen(5)                 # Now wait for client connection.
+updates = queue.Queue()
 
-while True:
-	connection, address = sock.accept()     # Establish connection with client.
-	print('Got connection from', address)
-	connection.send('Thank you for connecting')
-	connecting.close()
+
+def listen(ip, port):
+	
+	'''
+	Docstring goes here
+
+	'''
+
+	#
+	clients = []
+	running = True
+
+	# Create and bind socket for incoming connections
+	sock = socket.socket() # TODO: Arguments to constructor (?)
+	sock.bind((ip, port))
+	sock.listen(5)
+
+	# TODO: Managing clients after connection
+	# TODO: Quitting gracefully
+
+	#
+	while running:
+		client = sock.accept() # Client connection (conn, addr)
+		clients.append(client)
+		print('Accepted client #{0:}: {1:}'.format(len(clients), client[1]))
+		clientFork(*client)
+
+	sock.close()
+
+
+def clientFork(conn, addr):
+
+	'''
+	Docstring goes here
+
+	'''
+
+	print('Creating a new client process for {0:}.'.format(addr))
+
+
+	fill = '#%02x%02x%02x' % (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+	height = 0 #random.randint(20, 125)
+	cur = root.canvas.create_oval((0,0,0,0), fill=fill)
+
+	def protocol(conn, addr):
+
+		# TODO: Separate IO and logic
+
+		# Communication logic
+		try:
+			size = int(conn.recv(4).decode('utf-8')) 	# Read size prefix (padded to four digits)
+			received = conn.recv(size) 					# Read data
+		except:
+			print('Lost connection with {0:}.'.format(addr))
+			return False
+
+		data = pickle.loads(received)
+		print('Server received {0:} bytes from {1:}.'.format(size, addr)) # TODO: Print representation of incoming data (?)
+
+		# Application logic
+		if isinstance(data, str) and data.lower() in ('quit', 'q', 'exit', 'stop', 'no more', 'please', 'leave me alone', 'terminate'):
+			print('Terminating client process ({0:}).'.format(addr))
+			return False
+		else:
+			for point in data:
+				if point[0] == 'cur':
+					x, y = point[1]
+					root.canvas.coords(cur, (x-10, y-10, x+10, y+10))
+				else:
+					updates.put((point, fill, height), block=False)
+			return True
+
+		# root.after(1000//30, lambda: protocol(conn, addr)) # Run again after a set amount of time
+
+	def program(conn, addr):
+		running = protocol(conn, addr)
+		while running:
+			print('Running protocol')
+			running = protocol(conn, addr)
+
+	threading.Thread(target=program, args=(conn, addr)).start()
+
+
+
+def main():
+
+	'''
+	Docstring goes here
+
+	'''
+	
+	threading.Thread(target=listen, args=('localhost', 44000)).start()
+
+	def pollUpdates():
+		while not updates.empty():
+			print('Polling updates')
+			point, fill, height = updates.get(block=False)
+			print('Point is a', type(point))
+			root.canvas.create_oval((point[0], point[1]+height, point[0]+15, point[1]+15+height), fill=fill, width=0)
+		root.after(1000//30, pollUpdates)
+
+	pollUpdates()
+
+	root.mainloop()
+
+if __name__ == '__main__':
+	main()
